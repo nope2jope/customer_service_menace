1443bcc9ea83d927044cdc51673b581cb603060e
nope2jope <67401076+nope2jope@users.noreply.github.com>
2021-03-28 11:51:34 -0700

    streamlining, fuller functionality
    built-out classes, completed rough functionality, streamlined visual readability

--- BrazenSpeedBot.py
+++ BrazenSpeedBot.py
@@ -1,27 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import os
 import time
 
-DRIVER_PATH = os.environ['ENV_DRIVER_PATH']
-
-TWITTER_EMAIL = os.environ['ENV_EMAIL']
-TWITTER_PASSWORD = os.environ['ENV_PASSWORD']
-TWITTER_URL = 'https://www.twitter.com'
-SPEED_TEST_URL = 'https://www.speedtest.net/'
-
-PLEDGED_UPS = 5.0
-PLEDGED_DOWNS = 100.0
-
-ALERT_MESSAGE = f'Hey @Xfinity @Comcast. Currently getting {00}MBPS⬇️/{00}MBPS⬆️' \
-                f'in Bellingham, WA despite paying for {PLEDGED_DOWNS}MBPS⬇️/{PLEDGED_UPS}MBPS⬆️' \
-                f'What gives!?'
-
 class TestBot:
-    def __init__(self):
-        self.email = TWITTER_EMAIL
-        self.password = TWITTER_PASSWORD
-        self.driver = webdriver.Chrome(DRIVER_PATH)
+    def __init__(self, email, password, driver):
+        self.email = email
+        self.password = password
+        self.driver = webdriver.Chrome(driver)
         self.present_ups = None
         self.present_downs = None
 
@@ -43,26 +28,23 @@ class TestBot:
             '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span')
         self.present_ups = ups.text
 
+        time.sleep(5)
+
         results = [float(self.present_downs), float(self.present_ups)]
         return results
 
-    def alert_provider(self):
+    def alert_provider(self, tweet_url, message):
         driver = self.driver
-        driver.get(TWITTER_URL)
+        driver.get(tweet_url)
 
-        # login_button = driver.find_element_by_css_selector('button ')
-        # login_button.click()
         time.sleep(5)
 
         driver.find_element_by_link_text("Log in").click()
 
         time.sleep(5)
 
-        inputs = driver.find_elements_by_tag_name('input')
-
         email_input = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[1]/label/div/div[2]/div/input')
         email_input.send_keys(self.email)
-        #
 
         pw_input = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[2]/label/div/div[2]/div/input')
         pw_input.send_keys(self.password)
@@ -70,11 +52,14 @@ class TestBot:
 
         time.sleep(5)
 
-        twitter_field = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]')
-        twitter_field.send_keys(ALERT_MESSAGE)
+        bird_box = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div')
+        bird_box.click()
+        bird_box.send_keys(message)
 
+        tweet_button = driver.find_element_by_xpath('//*[@data-testid="tweetButtonInline"]')
 
+        time.sleep(5)
 
-tester = TestBot()
+        tweet_button.click()
 
-tester.alert_provider()
+        time.sleep(5)
--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from BrazenSpeedBot import TestBot
+import time
 import os
 
 DRIVER_PATH = os.environ['ENV_DRIVER_PATH']
 
-TWITTER_USERNAME = os.environ['ENV_USERNAME']
+TWITTER_EMAIL = os.environ['ENV_EMAIL']
 TWITTER_PASSWORD = os.environ['ENV_PASSWORD']
 
 PLEDGED_UPS = 5
 PLEDGED_DOWNS = 100
 
+TWITTER_URL = 'https://www.twitter.com'
+SPEED_TEST_URL = 'https://www.speedtest.net/'
+
+tester = TestBot(email=TWITTER_EMAIL, password=TWITTER_PASSWORD, driver=DRIVER_PATH)
+
+test_results = tester.test_connection(test_url=SPEED_TEST_URL)
+
+if test_results[0] < PLEDGED_DOWNS or test_results[1] < PLEDGED_UPS:
+    present_downs = test_results[0]
+    present_ups = test_results[1]
+    alert_message = f'Hey @Xfinity @Comcast. Currently getting {present_downs}MBPS⬇️/{present_ups}MBPS⬆️' \
+                    f'in Bellingham, WA despite paying for {PLEDGED_DOWNS}MBPS⬇️/{PLEDGED_UPS}MBPS⬆️' \
+                    f'What gives!?'
+    tester.alert_provider(tweet_url=TWITTER_URL, message=alert_message)
+    time.sleep(5)
+    tester.driver.close()
+else:
+    time.sleep(5)
+    tester.driver.close()
+    print('Phew! The free market never disappoints!')
 
